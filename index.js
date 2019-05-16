2f118204691098c77013c515b6e5483717c4eb58
Claudio Spizzi <claudio@spizzi.net>
2019-05-16 23:58:56 +0200

    Redesign mqtt2loxone

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 #!/usr/bin/env node
 
+
+/**
+ * SETUP
+ */
+
+// Global modules
 const log = require('yalm');
 const mqtt = require('mqtt');
-const express = require('express');
-const bodyParser = require('body-parser');
+const dgram = require('dgram');
+const request = require('request');
+const encodeurl = require('encodeurl')
 
 const pkg = require('./package.json');
 const cfg = require(process.argv[2] || './config.json');
 
-let mqttConnected;
-
 log.setLevel(cfg.log);
 log.info(pkg.name + ' ' + pkg.version + ' starting');
+
+
+/**
+ * SETUP MQTT
+ */
+
+let mqttConnected;
 
 const mqttClient = mqtt.connect(
     cfg.mqtt.url, {
@@ -27,7 +39,7 @@ mqttClient.on('connect', () => {
     mqttConnected = true;
     log.info('mqtt: connected ' + cfg.mqtt.url);
 
-    // ToDo: Subscribe
+    mqttClient.subscribe(cfg.mqtt.name + '/set/#');
 });
 
 mqttClient.on('close', () => {
@@ -45,29 +57,107 @@ mqttClient.on('error', err => {
 
 mqttClient.on('message', (topic, payload, msg) => {
 
-    // ToDo: Parse Messages
+    log.info('mqtt: message ' + topic + ' ' + payload.toString());
+
+    // Try to parse the payload. If not possible, add null as payload.
+    try {
+        payload = JSON.parse(payload.toString());
+    }
+    catch {
+        payload = {
+            val: null,
+            name: 'unknown'
+        };
+    }
+
+    // Else use the udp datagram api for non-text like bool, number, null.
+    if (typeof(payload.val) != 'string') {
+
+        let message = topic;
+        if (payload.val != null) {
+            message += '=' + payload.val;
+        }
+
+        log.info('udp client: send datagram ' + message);
+
+        const udpClient = dgram.createSocket('udp4');
+        udpClient.send(message, cfg.loxone.port, cfg.loxone.host, (err) => {
+            udpClient.close();
+        });
+    }
+    
+    // Use http, if the payload is a text value.
+    if (typeof(payload.val) == 'string') {
+
+        let url = encodeurl('http://' + cfg.loxone.username + ':' + cfg.loxone.password + '@' + cfg.loxone.host + '/dev/sps/io/' + payload.name + '/' + payload.val);
+
+        log.info('http client: invoke request http://' + cfg.loxone.host + '/dev/sps/io/' + payload.name + '/' + payload.val);
+
+        request(url, (error, response, body) => {
+
+            if (error) {
+                log.error(error);
+            }
+        });
+    }
 });
 
-const expressServer = express();
-expressServer.use(bodyParser.text({ type: 'text/*' }));
 
-expressServer.route('/mqtt/:topic(*)')
-    .post(parseRequest);
+/**
+ * UDP SERVER
+ */
 
-expressServer.listen(cfg.loxone.port, '0.0.0.0', () => {
-    log.info('express: server running on http://0.0.0.0:' + cfg.loxone.port);
+const udpServer = dgram.createSocket('udp4');
+
+udpServer.on('listening', () => {
+
+    log.info('udp server: listen on udp://' + udpServer.address().address + ':' + udpServer.address().port);
 });
 
-function parseRequest(req, res) {
+udpServer.on('close', () => {
 
-    publishMqttStatus(req.params.topic, req.body);
-    res.send();
-}
+    log.info('udp server: closed');
+});
 
-function publishMqttStatus(topic, value) {
+udpServer.on('message', (message, remote) => {
 
+    log.info('udp server: message from udp://' + remote.address + ':' + remote.port + ' => ' + message);
+
+    let messageParts = message.toString().split(';');
+
+    // Define topic. This must be in the udp message.
+    let topic = cfg.mqtt.name + '/' + messageParts[0];
+
+    // Define value. Can be null or empty.
+    let value = '';
+    if (messageParts.length > 1) {
+        value = messageParts[1];
+    }
+
+    // Define the mqtt qos. Default is 0.
+    let qos = 0;
+    if (messageParts.length > 2) {
+        qos = parseInt(messageParts[2]);
+    }
+
+    // Define the mqtt retain. Default is false.
+    let retain = false;
+    if (messageParts.length > 3) {
+        retain = messageParts[3] == 'true';
+    }
+
+    // Define the optional name payload string. Default is not defined.
+    let name = null;
+    if (messageParts.length > 4) {
+        name = messageParts[4];
+    }
+
+    // Parse the value, to publish the correct format.
     let parsedValue;
-    if (value == 'true') {
+    if (value == '') {
+        parsedValue = '';
+    }
+    else if (value == 'true') {
         parsedValue = 1;
     }
     else if (value == 'false') {
@@ -80,11 +170,22 @@ function publishMqttStatus(topic, value) {
         parsedValue = value;
     }
 
+    // Prepare the payload object with timestamp, value and optionally the name.
     payload = {
         ts: Date.now(),
         val: parsedValue
     }
+    if (name !== null) {
+        payload.name = name;
+    }
 
-    mqttClient.publish(topic, JSON.stringify(payload));
+    mqttClient.publish(topic, JSON.stringify(payload), { qos: qos, retain: retain });
     log.info('mqtt: publish ' + topic + ' ' + JSON.stringify(payload));
-}
+});
+
+udpServer.on('error', (err) => {
+
+    log.error(err);
+});
+
+udpServer.bind(cfg.loxone.port);
