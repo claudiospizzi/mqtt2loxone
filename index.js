49f4170d23afec2325ce7fe43466a9775285116f
Claudio Spizzi <claudio@spizzi.net>
2019-08-25 21:05:44 +0200

    Update code for eslint, add feature to pass through non-json payload

--- index.js
+++ index.js
@@ -1,209 +1,210 @@
 #!/usr/bin/env node
 
-
 /**
  * SETUP
  */
 
 // Global modules
-const log = require('yalm');
-const mqtt = require('mqtt');
-const dgram = require('dgram');
-const request = require('request');
+const log = require('yalm')
+const mqtt = require('mqtt')
+const dgram = require('dgram')
+const request = require('request')
 const encodeurl = require('encodeurl')
 
-const pkg = require('./package.json');
-const cfg = require(process.argv[2] || './config.json');
+const pkg = require('./package.json')
+const cfg = require(process.argv[2] || './config.json')
 
-log.setLevel(cfg.log);
-log.info(pkg.name + ' ' + pkg.version + ' starting');
-
+log.setLevel(cfg.log)
+log.info(pkg.name + ' ' + pkg.version + ' starting')
 
 /**
  * SETUP MQTT
  */
 
-let mqttConnected;
+let mqttConnected
 
 const mqttClient = mqtt.connect(
     cfg.mqtt.url, {
         will: { topic: cfg.mqtt.name + '/connected', payload: '0', retain: true },
         rejectUnauthorized: cfg.mqtt.secure
     }
-);
+)
 
 mqttClient.on('connect', () => {
 
-    mqttClient.publish(cfg.mqtt.name + '/connected', '2', { retain: true });
+    mqttClient.publish(cfg.mqtt.name + '/connected', '2', { retain: true })
 
-    mqttConnected = true;
-    log.info('mqtt: connected ' + cfg.mqtt.url);
+    mqttConnected = true
+    log.info('mqtt: connected ' + cfg.mqtt.url)
 
-    mqttClient.subscribe(cfg.mqtt.name + '/set/#');
+    mqttClient.subscribe(cfg.mqtt.name + '/set/#')
 
-    for (let subscriptionKey in cfg.loxone.subscriptions) {
-        mqttClient.subscribe(cfg.loxone.subscriptions[subscriptionKey]);
+    for (const subscriptionKey in cfg.loxone.subscriptions) {
+        mqttClient.subscribe(cfg.loxone.subscriptions[subscriptionKey])
     }
-});
+})
 
 mqttClient.on('close', () => {
 
     if (mqttConnected) {
-        mqttConnected = false;
-        log.info('mqtt: disconnected ' + cfg.mqtt.url);
+        mqttConnected = false
+        log.info('mqtt: disconnected ' + cfg.mqtt.url)
     }
-});
+})
 
 mqttClient.on('error', err => {
 
-    log.error('mqtt: error ' + err.message);
-});
+    log.error('mqtt: error ' + err.message)
+})
 
 mqttClient.on('message', (topic, payload, msg) => {
 
-    log.info('mqtt: message ' + topic + ' ' + payload.toString());
+    log.info('mqtt: message ' + topic + ' ' + payload.toString())
 
     // Try to parse the payload. If not possible, add null as payload.
     try {
-        payload = JSON.parse(payload.toString());
-    }
-    catch {
-        payload = {
-            val: null,
-            name: 'unknown'
-        };
+        payload = JSON.parse(payload.toString())
+    } catch (err) {
+        if (!isNaN(payload.toString())) {
+            payload = {
+                val: Number(payload.toString()),
+                name: 'unknown'
+            }
+        } else {
+            payload = {
+                val: null,
+                name: 'unknown'
+            }
+        }
     }
 
-    // Else use the udp datagram api for non-text like bool, number, null.
-    if (typeof(payload.val) != 'string') {
+    // Use the udp datagram api for non-text like bool, number, null.
+    if (typeof (payload.val) !== 'string') {
 
-        let message = topic;
+        let message = topic
         if (payload.val != null) {
-            message += '=' + payload.val;
+            message += '=' + payload.val
         }
 
-        log.info('udp client: send datagram ' + message);
+        log.info('udp client: send datagram ' + message)
 
-        const udpClient = dgram.createSocket('udp4');
-        udpClient.send(message, cfg.loxone.port, cfg.loxone.host, (err) => {
-            udpClient.close();
-        });
+        const udpClient = dgram.createSocket('udp4')
+        udpClient.send(message, cfg.loxone.port, cfg.loxone.host, (error) => {
+            if (error) {
+                log.error('udp client error: ' + error)
+            }
+            udpClient.close()
+        })
     }
-    
+
     // Use http, if the payload is a text value.
-    if (typeof(payload.val) == 'string') {
+    if (typeof (payload.val) === 'string') {
 
-        let url = encodeurl('http://' + cfg.loxone.username + ':' + cfg.loxone.password + '@' + cfg.loxone.host + '/dev/sps/io/' + payload.name + '/' + payload.val);
+        const url = encodeurl('http://' + cfg.loxone.username + ':' + cfg.loxone.password + '@' + cfg.loxone.host + '/dev/sps/io/' + payload.name + '/' + payload.val)
 
-        log.info('http client: invoke request http://' + cfg.loxone.host + '/dev/sps/io/' + payload.name + '/' + payload.val);
+        log.info('http client: invoke request http://' + cfg.loxone.host + '/dev/sps/io/' + payload.name + '/' + payload.val)
 
         request(url, (error, response, body) => {
-
             if (error) {
-                log.error(error);
+                log.error('http client error: ' + error)
             }
-        });
+        })
     }
-});
-
+})
 
 /**
  * UDP SERVER
  */
 
-const udpServer = dgram.createSocket('udp4');
+const udpServer = dgram.createSocket('udp4')
 
 udpServer.on('listening', () => {
 
-    log.info('udp server: listen on udp://' + udpServer.address().address + ':' + udpServer.address().port);
-});
+    log.info('udp server: listen on udp://' + udpServer.address().address + ':' + udpServer.address().port)
+})
 
 udpServer.on('close', () => {
 
-    log.info('udp server: closed');
-});
+    log.info('udp server: closed')
+})
 
 udpServer.on('message', (message, remote) => {
 
-    message = message.toString().trim();
+    message = message.toString().trim()
 
-    log.info('udp server: message from udp://' + remote.address + ':' + remote.port + ' => ' + message);
+    log.info('udp server: message from udp://' + remote.address + ':' + remote.port + ' => ' + message)
 
-    let messageParts = message.split(';');
+    let messageParts = message.split(';')
 
     // Check if the message was send by the logger or by the UDP virtual output
     // and concatenate the array if it's the logger.
-    let regexLogger = /^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2};.*$/g;
+    const regexLogger = /^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2};.*$/g
     if (message.match(regexLogger) != null) {
-        messageParts = messageParts.splice(2);
+        messageParts = messageParts.splice(2)
     }
 
     // Define topic. This must be in the udp message.
-    let topic = messageParts[0];
+    let topic = messageParts[0]
 
     // Define value. Can be null or empty.
-    let value = '';
+    let value = ''
     if (messageParts.length > 1) {
-        value = messageParts[1];
+        value = messageParts[1]
     }
 
     // Define the mqtt qos. Default is 0.
-    let qos = 0;
+    let qos = 0
     if (messageParts.length > 2) {
-        qos = parseInt(messageParts[2]);
+        qos = parseInt(messageParts[2])
     }
 
     // Define the mqtt retain. Default is false.
-    let retain = false;
+    let retain = false
     if (messageParts.length > 3) {
-        retain = messageParts[3] == 'true';
+        retain = messageParts[3] === 'true'
     }
 
     // Define the optional name payload string. Default is not defined.
-    let name = null;
+    let name = null
     if (messageParts.length > 4) {
-        name = messageParts[4];
+        name = messageParts[4]
     }
 
     // Add the default prefix if the custom prefix is not specified
-    if (messageParts.length < 6 || (messageParts.length > 5 && messageParts[5] == 'true')) {
-        topic = cfg.mqtt.name + '/' + topic;
+    if (messageParts.length < 6 || (messageParts.length > 5 && messageParts[5] === 'true')) {
+        topic = cfg.mqtt.name + '/' + topic
     }
 
     // Parse the value, to publish the correct format.
-    let parsedValue;
-    if (value == '') {
-        parsedValue = '';
-    }
-    else if (value == 'true') {
-        parsedValue = 1;
-    }
-    else if (value == 'false') {
-        parsedValue = 0;
-    }
-    else if (!isNaN(value)) {
-        parsedValue = Number(value);
-    }
-    else {
-        parsedValue = value;
+    let parsedValue
+    if (value === '') {
+        parsedValue = ''
+    } else if (value === 'true') {
+        parsedValue = 1
+    } else if (value === 'false') {
+        parsedValue = 0
+    } else if (!isNaN(value)) {
+        parsedValue = Number(value)
+    } else {
+        parsedValue = value
     }
 
     // Prepare the payload object with timestamp, value and optionally the name.
-    payload = {
+    const payload = {
         ts: Date.now(),
         val: parsedValue
     }
     if (name !== null) {
-        payload.name = name;
+        payload.name = name
     }
 
-    mqttClient.publish(topic, JSON.stringify(payload), { qos: qos, retain: retain });
-    log.info('mqtt: publish ' + topic + ' ' + JSON.stringify(payload));
-});
+    mqttClient.publish(topic, JSON.stringify(payload), { qos: qos, retain: retain })
+    log.info('mqtt: publish ' + topic + ' ' + JSON.stringify(payload))
+})
 
 udpServer.on('error', (err) => {
 
-    log.error(err);
-});
+    log.error(err)
+})
 
-udpServer.bind(cfg.loxone.port);
+udpServer.bind(cfg.loxone.port)
